6ce75237a49b0fd29b3b3d3aac1f8d99bf30a1df
mllmale <leticiacle2012@hotmail.com>
2025-03-25 19:57:54 -0300

    implementado user

--- src/main/java/com/Tarefas/to_do_list/domain/User.java
+++ src/main/java/com/Tarefas/to_do_list/domain/User.java
@@ -1,4 +1,20 @@
 package com.Tarefas.to_do_list.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String email;
+    private String senha;
 }
