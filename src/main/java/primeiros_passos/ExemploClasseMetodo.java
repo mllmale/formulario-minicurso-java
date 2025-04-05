5af44043f392d3ca42b06aff4527538461aa1f2c
mllmale <leticiacle2012@hotmail.com>
2025-04-05 12:45:56 -0300

    branch certa

---
+++ src/main/java/primeiros_passos/ExemploClasseMetodo.java
@@ -0,0 +1,49 @@
+package primeiros_passos;
+
+class Pessoa {
+    private String nome;
+    private int idade;
+
+    // Construtor
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    // Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    // Método decorado (simulando "decorator" como prefixo/sufixo)
+    public String apresentar() {
+        return "*** " + nome.toUpperCase() + " - " + idade + " anos ***";
+    }
+}
+
+public class ExemploClasseMetodo {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("Letícia", 21);
+
+        // Usando getters
+        System.out.println("nome: " + p1.getNome());
+        System.out.println("Idade: " + p1.getIdade());
+
+        // Apresentação com "decorator"
+        System.out.println(p1.apresentar());
+    }
+}
+
